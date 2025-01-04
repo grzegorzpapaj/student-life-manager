00bbd1b0b30fef5907a6c97e1fc798c2e7e04dfa
grzegorzpapaj <137798629+grzegorzpapaj@users.noreply.github.com>
2025-01-04 16:10:09 +0100

    Initialize Spring Boot project

---
+++ src/main/java/com/gpaqd/student_life_manager/StudentLifeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gpaqd.student_life_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLifeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLifeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpaqd/student_life_manager/StudentLifeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpaqd.student_life_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLifeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
