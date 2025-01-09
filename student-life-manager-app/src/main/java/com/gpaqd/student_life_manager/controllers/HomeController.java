e0efbb4d9ac5bce187a7f65bc0f6ba776d8f6bf9
grzegorzpapaj <137798629+grzegorzpapaj@users.noreply.github.com>
2025-01-09 18:57:13 +0100

    Add user dao, save test user

--- student-life-manager-app/src/main/java/com/gpaqd/student_life_manager/controllers/HomeController.java
+++ student-life-manager-app/src/main/java/com/gpaqd/student_life_manager/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.gpaqd.student_life_manager.controllers;
 
+import com.gpaqd.student_life_manager.dao.UserDAOImpl;
+import com.gpaqd.student_life_manager.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
+	private UserDAOImpl userDAO;
+
+	@Autowired
+	public HomeController(UserDAOImpl userDAO) {
+		this.userDAO = userDAO;
+	}
+
 	@GetMapping
 	public String home() {
-		return "Welcome to Student Life Manager";
+
+		System.out.println("Creating new user object...");
+		User user = new User("abcd", "1234");
+
+		System.out.println("Saving the user");
+		userDAO.save(user);
+
+		return "Saved user: " + user.getUsername();
 	}
 }
---
+++ student-life-manager-app/src/main/java/com/gpaqd/student_life_manager/dao/UserDAOImpl.java
@@ -0,0 +1,24 @@
+package com.gpaqd.student_life_manager.dao;
+
+import com.gpaqd.student_life_manager.entity.User;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class UserDAOImpl implements UserDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public UserDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+}
